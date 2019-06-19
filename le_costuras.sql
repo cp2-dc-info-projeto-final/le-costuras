aa78ca077599cadcc8ddfb5817418c391bb41c08
Caio Borges <santoscaiovascaino@gmail.com>
2019-06-19 09:01:48 -0300

    criando banco de dados

---
+++ le_costuras.sql
@@ -0,0 +1,57 @@
+use lecosturas;
+
+CREATE TABLE cliente (
+    id int NOT null PRIMARY KEY AUTO_INCREMENT,
+    nome varchar (100) NOT null,
+    cpf varchar (14) NOT null,
+    datadenascimento date NOT null,
+    email varchar (200) NOT null,
+    senha varchar (20) not null,
+    endereco varchar (100) NOT null
+    );
+    
+CREATE TABLE produto(
+    id int NOT null PRIMARY KEY AUTO_INCREMENT,
+    nome varchar (50) NOT null,
+    descricao varchar (200) NOT null,
+    tamanho varchar (10) NOT null,
+    preco float NOT null
+    );
+    
+CREATE TABLE catalogo(
+    id int NOT null PRIMARY KEY AUTO_INCREMENT,
+    produto int,
+    FOREIGN key (id) REFERENCES produto (id)
+    );
+    
+CREATE TABLE compra(
+    id int NOT null PRIMARY KEY AUTO_INCREMENT,
+    valor float NOT null,
+    formapagamento varchar (20) not null,
+    endereco varchar (100) not null
+    );
+    
+CREATE TABLE carrinho (
+    id int NOT null PRIMARY KEY AUTO_INCREMENT,
+    produto int,
+    quantidade int not null,
+    valortotal float NOT null,
+    FOREIGN KEY (id) REFERENCES produto (id)
+    );
+    
+ CREATE TABLE encomenda (
+     id int NOT null PRIMARY KEY AUTO_INCREMENT,
+     medidas varchar (10) NOT null,
+     descricao varchar (300) NOT null
+     );
+   
+ CREATE TABLE administrador (
+     id int NOT null PRIMARY KEY AUTO_INCREMENT,
+     nome varchar (100) NOT null,
+    cpf varchar (14) NOT null,
+    datadenascimento date NOT null,
+    email varchar (200) NOT null,
+    senha varchar (20) not null,
+    endereco varchar (100) NOT null
+    );
+     
