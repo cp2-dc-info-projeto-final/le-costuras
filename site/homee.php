aada09b444e45ee616a24889e1d2ea952808663a
Marcella Moura <43192194+marcellamoura1@users.noreply.github.com>
2019-09-23 08:27:26 -0300

    Update homee.php

--- site/homee.php
+++ site/homee.php
@@ -111,6 +111,36 @@ function openCity(cityName,elmnt,color) {
 document.getElementById("defaultOpen").click();
 </script>
 
+<br><br><br><br><br>
+
+<center>
+<h2 class="w3-center">le costuras</h2>
+
+
+<div class="w3-content w3-section" style="max-width:850px">
+  <img class="mySlides" src="pizza.jpg" style="width:100%">
+  <img class="mySlides" src="pastel.jpg" style="width:100%">
+  <img class="mySlides" src="fritas.jpg" style="width:100%">
+</div>
+</center>
+
+<script>
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+</script>
+
 </body>
 </html> 
 
