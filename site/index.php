19d6da2d5c7582b2bb924ace454f9195dee7c6b9
Lab Caxias <cp2dcds@outlook.com>
2019-10-11 07:39:23 -0300

    indexcarrinho

---
+++ site/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>LêCosturas</title>
+</head>
+<body>
+<link href="formulario.css" rel="stylesheet"> <!--link com o arquivo css para estilizar o site-->
+<h1> LêCosturas</h1> 
+
+<br>
+<nav id="menu" inline >
+        <ul>
+        <li><a  href="home.php">Home</a></li>
+            <li><a href="">Catálogo</a>
+                <ul>
+                        <li><a href="femininioview.php">Feminino</a></li>
+                        <li><a href="maculinoview.php">Masculino</a></li>
+                        <li><a href="infantilview.php">Infantil</a></li>
+                        <li><a href="#">Promoções</a></li>                    
+                  </ul>
+              </li>
+            <li><a href="loginview.php">Login</a></li>
+            <li><a  href="cadastroview.php">Cadastro</a></li>
+            <li><a href="#">Carrinho</a></li>
+        </ul>
+        
+    </nav>
+</body>
+</html>
+    <?php
+    session_start();
+    if(array_key_exists('nome', $_SESSION) == false){
+        $erro = "Acesso Negado!";        
+        $_SESSION["erro"] = $erro;
+        header('Location: loginview.php');
+        exit();
+    } else {
+        $nome = $_SESSION["nome"];
+        echo "Seja bem vindo $nome!<br>";
+        echo "<a href='sair.php'>Sair</a>";
+    }
+?>
--- site/lecostura.sql
+++ site/lecostura.sql
@@ -1,10 +1,3 @@
-CREATE TABLE produto (
-	id int PRIMARY KEY  AUTO_INCREMENT,
-	nome	varchar(100),	
-    preco	float,	
-	descricao	longtext
-);
-
 CREATE TABLE usuario (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100),
@@ -14,13 +7,29 @@ CREATE TABLE usuario (
     endereco VARCHAR (100)
 );
 
-CREATE TABLE carrinho (
-    id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    cod INT(10),
-    nome VARCHAR(150),
-    preco float,
-    qtd INT(20),
-    sessao TEXT
-                                                                                                                                         -
+CREATE TABLE carrinho_produtos (
+	produto_id INT PRIMARY KEY  AUTO_INCREMENT,
+	produto_nome VARCHAR(100),	
+    produto_preco DECIMAL(3,2),	
+	produto_descricao TEXT
 );
 
+CREATE TABLE carrinho_pedidos (
+    pedido_id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    pedido_produto INT(10),
+    pedido_qtde INT(10),
+    pedido_preco DECIMAL(3,2),
+    pedido_cliente VARCHAR (255),
+    pedido_data DATETIME,
+    pedido_sessao INT(10)
+);
+
+CREATE TABLE carrinho_temporario (
+    temporario_id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    temporario_produto INT(10),
+    temporario_qtde INT(10),
+    temporario_preco DECIMAL(3,2),
+    temporario_cliente VARCHAR (255),
+    temporario_data DATETIME,
+    temporario_sessao INT(10)
+);
