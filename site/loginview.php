a891afca97f5b2076eaa5de0e556d21915885113
kah750 <kah750@gmail.com>
2019-11-13 23:36:31 -0300

    Login - View

--- site/loginview.php
+++ site/loginview.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>Login</title>
+  <meta charset="UTF-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">	
+	  <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	  <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	  <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="formulario.css">
+    
 </head>
+
+
 <body>
-<link href="formulario.css" rel="stylesheet"> <!--link com o arquivo css para estilizar o site-->
-<h1> LêCosturas</h1> 
 
 <br>
-<nav id="menu" inline >
+<nav class="menu">
         <ul>
             <li><a  href="home.php">Home</a></li>
-            <li><a href="">Catálogo</a>
-               
-              </li>
-            <li><a href="loginview.php">Login</a></li>
+            <li><a href="">Catálogo</a></li>
             <li><a  href="cadastroview.php">Cadastro</a></li>
             <li><a href="#">Carrinho</a></li>
         </ul>
@@ -33,34 +39,43 @@
         echo "$erro";
         }
     ?>
-      <h4>              -    </h4>
-    <h2>Login</h2>
-<br>    <div >
-        <label for="email">E-mail:</label>
-        <input class="text" type="email" id="email"  name="email" />
-    </div>
-<br>
-    <div >
-        <label for="senha">Senha:</label>
-        <input class="text" type="password" id="senha"  name="senha" />
-    </div>
-    <br>
-    <div class= "checkbox">
-    <input type="checkbox" id="Login" name="Login" checked> Mantenha-me logado </div>
-    <div class="button">
-    <button type="submit" >Entrar</button>
-    </div>
-    <div>
-       
-        
+    <div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-33">
+						Login
+					</span>
 
-      </div>
-  <br>
-  <a href="">Esqueci minha senha</a>
-  <br>
-  
-  <a href="cadastroview.php"> Cadastrar </a> <!--link com a pagina de cadastro-->
-  <h3>           -     </h4>
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="Email">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+
+					<div class="wrap-input100 rs1 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="senha" placeholder="Senha">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-20">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center p-t-45 p-b-4">
+						
+
+						<a href="#" class="txt2 hov1">
+							Cadastre-se
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
   </form>
  
 </body>
