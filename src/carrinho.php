4f266ea103cfdb9d4a180b28c84844ecda6c0078
Emanuel Pires <ueitomep@gmail.com>
2019-11-21 12:56:29 -0300

    carrinho

--- src/carrinho.php
+++ src/carrinho.php
@@ -4,6 +4,8 @@
         <th>Produto</th>
         <th>Preco<th>
         <th>Quantidade</th>
+        <th>Adicionar Peça</th>
+        <th>Remover Peça</th>
     </tr>
 
 
@@ -34,10 +36,18 @@
     echo $produto['nome']?></td>
             <td> R$ <?php echo number_format ($produto['preco'], 2, ',', '.')?></td>
             <td> <?php echo $qtd; ?></td>
+            <td><button class="button"><a href="adicionarCarrinho.php?prod=<?php 
+         echo $produto['id']?>" class="color-white">Adicionar Peça</a> </button></td>
+          <td><button class="button"><a href="tirarcarrinho.php?prod=<?php 
+         echo $produto['id']?>" class="color-white">Remover Peça</a> </button>
     </tr>
+   
+
     
     <?php 
 } }
     ?>
 
 </table>
+
+<button class="button"><a href="indexcarrinho.php" class="color-white">Voltar Ao Catálogo</a> </button>
