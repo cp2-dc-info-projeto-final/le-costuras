ae134ab5417281edcd686a081c5a549cc5ccfad9
Emanuel Pires <ueitomep@gmail.com>
2019-11-21 12:11:11 -0300

    carrinho

--- src/carrinho.php
+++ src/carrinho.php
@@ -11,7 +11,7 @@
         session_start();
     include 'conexaocarrinho.php';
     $pdo = criarConexao();
-                          
+    
     
     $consulta = $pdo->prepare("SELECT * FROM produto");
     $consulta->execute();
@@ -25,16 +25,19 @@
    
    foreach($_SESSION["carrinho"] as $id => $qtd)
    foreach($produtos as $produto){
-            
+    if($produto['id'] == $id){
 ?>
 
     <tr>
-    <td> <?php echo $produto['nome']?></td>
-            <td> <?php echo $produto['preco']?></td>
+    <td> <?php 
+    
+    echo $produto['nome']?></td>
+            <td> R$ <?php echo number_format ($produto['preco'], 2, ',', '.')?></td>
             <td> <?php echo $qtd; ?></td>
     </tr>
+    
     <?php 
-} 
+} }
     ?>
 
 </table>
