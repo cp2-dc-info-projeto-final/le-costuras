4c39d0df0942dae4543966a8b068f35e1ee2aec4
mileny <milenytcc@gmail.com>
2019-11-22 16:36:18 -0300

    .

--- src/encomenda.php
+++ src/encomenda.php
@@ -1,3 +1,52 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+       <meta charset="utf-8">
+       <title> Encomendas </title>
+       <link href="C:\Users\labcaxias\Desktop\electron-packager-master">
+       <link href="carrinho.css" rel="stylesheet">
+       <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+       <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+       <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+       <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+       <link rel="stylesheet" type="text/css" href="css2/style.css">
+       <link rel="stylesheet" type="text/css" href="css2/memenu.css">
+       <link rel="sortcut icon" href="logo.png" type="image/x-icon" />
+       
+       
+       </head>
+
+       <body>
+    <section class="menusuperior">
+       <div class=" h_menu4">
+				<ul class="memenu skyblue">
+					  <li><a class="color1" href="indexcarrinho.php">Catálogo</a></li>	
+				
+				<li><a  href="loginview.php">Login</a></li>	
+				<?php
+				  if (isset($_SESSION['email'])&& $_SESSION['moderador']==1){
+					  ?>
+				<li><a  href="paginaadmview.php">Pagina do Administrador</a></li>
+				 <?php } ?>
+				 <li><a  href="homee.php">Home</a></li>
+				 <li><a  href="carrinho.php">Carrinho</a></li>
+				<li><a  href="cadastroview.php">Cadastro</a></li>
+				<li><a  href="sair.php">Sair</a></li>
+			  </ul> 
+			</div>
+          <br><br>
+
+   </section>
+   <section>
+          <header>
+
+
+             <h1 class="titulo">ENCOMENDAS</h1>    
+          </header>
+
+
+
+
 <?php
  require_once "conexao.php";
 
@@ -52,6 +101,7 @@
  foreach($vendas as $venda) {
 
 ?>
+
     <div>
         <?php echo $venda["id"]; ?><br>
         <?php echo $venda["nome"]; ?><br>
@@ -76,6 +126,7 @@
                 <td> <?php echo $produto["qtd"]; ?> </td>
                 <td> <?php echo $produto["preco"] * $produto["qtd"]; ?> </td>
             </tr>
+            </table>
 
             <?php
                 }    
